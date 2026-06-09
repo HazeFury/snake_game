b6e3668dea22a025385290f4215e52f7a265643b
HazeFury <mab.dev69@gmail.com>
2026-06-09 19:40:27 +0200

    basic menu done

--- src/main.py
+++ src/main.py
@@ -16,7 +16,8 @@ from views.menu import MenuView
 def main():
     """Main function"""
     # Create a window class. This is what actually shows up on screen
-    window = arcade.Window(title="Snake Game")
+    window = arcade.Window(title="Snake Game", width=1200, height=800)
+    window.set_location(x=800, y=1200)
 
     # Show the view on screen
     window.show_view(MenuView())
